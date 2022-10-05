ee8bfc9d0c822a2a00293580be3c8338fb39a7cc
Joshua P <jwpjr567@gmail.com>
2022-10-05 17:33:23 +0000

    feat: base project

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
 
-const { parse, read } = require("properties-parser");
+import { parse } from 'properties-parser';
+import fetch from 'node-fetch';
+import { spawn } from 'node:child_process';
 
-console.log(process.argv.slice(2));
-console.log(read("data/commands.properties"));
+let arg = process.argv.slice(2)[0];
 
+if (!arg) {
+  console.error('usage: installit [installer] - run an installer');
+  console.error('usage: installit list - list all installers');
+  process.exit(0);
+}
+
+arg = arg.toLowerCase();
+
+const response = await fetch('https://raw.githubusercontent.com/jwpjrdev/installit/master/data/commands.properties');
+const commands = parse(await response.text());
+
+const keys = Object.keys(commands);
+
+if (arg === 'list') {
+  const installers = keys.join(', ');
+  console.log(`available installers: ${installers}`);
+  process.exit(0);
+}
+
+if (!keys.includes(arg)) {
+  console.error(`installer '${arg}' not found`);
+  process.exit(0);
+}
+
+spawn(commands[arg], [], { shell: true, stdio: 'inherit' });
+// .on('exit', (code) => console.log(code.toString()));
